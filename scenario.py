abb9d63d47ed4e4d828c60e4bef326e702a38411
Daniel Hogan <6313241+dphogan@users.noreply.github.com>
2023-11-27 19:41:04 -0500

    End Llama.cpp output at triple quotation marks

--- scenario.py
+++ scenario.py
@@ -61,7 +61,7 @@ class LLM():
                 callback_manager=cbm,
                 verbose=False,
             )
-            self.bound = {}
+            self.bound = {'stop': '"""'}
 
         elif self.source_name == 'huggingface':
 
