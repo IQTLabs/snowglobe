fcd14a3a792550d4be62b1060392bebb5eec1f3f
Daniel Hogan <6313241+dphogan@users.noreply.github.com>
2023-12-05 00:27:13 -0500

    Nature on by default

--- scenario.py
+++ scenario.py
@@ -167,7 +167,7 @@ class Control():
         self.public_history.add(player_name, player_response)
 
     def adjudicate(self, history=None, responses=None, query=None,
-                   nature=False, timeframe='week', verbose=0):
+                   nature=True, timeframe='week', verbose=0):
         if query is None:
             #query = 'This is an example of what happens next as a result of these plans.'
             if nature:
@@ -352,7 +352,7 @@ class Player():
 
 
 def novel(llm):
-    template = 'History:\n"""\n{history}\n"""\n\nNews:\n"""\n{news}\n"""\n\nIn a short paragraph, what\'s the most important information appearing in the news but not in the history?  Do not use phrases like "most important news" in your answer:\n"""\n'
+    template = 'History:\n"""\n{history}\n"""\n\nNews:\n"""\n{news}\n"""\n\nIn a short paragraph, what\'s the most important information appearing in the news but not in the history?  Do not use phrases like "most important news" or "most important information" in your answer:\n"""\n'
     prompt = langchain.prompts.PromptTemplate(
         template=template,
         input_variables=['history', 'news'],
