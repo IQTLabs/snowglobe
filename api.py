773be65f92610ee4a5e41b4de85f48ea7ed3dada
Daniel Hogan <6313241+dphogan@users.noreply.github.com>
2024-01-05 15:16:46 -0500

    Beginning of terminal HTML page

--- api.py
+++ api.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 import fastapi
+import fastapi.staticfiles
 
 app = fastapi.FastAPI()
 
-@app.get('/')
+@app.get('/data')
 async def prompt():
     return {'message': 'testing 1 2 3'}
 
+app.mount('/', fastapi.staticfiles.StaticFiles(directory='terminal', html=True), name='terminal')
