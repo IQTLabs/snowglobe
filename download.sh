fad2300a9d75e0a542ccca10351dc94197904557
Daniel Hogan <6313241+dphogan@users.noreply.github.com>
2024-02-24 20:54:53 -0500

    Script to download jQuery and the default LLM

---
+++ download.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+wget https://code.jquery.com/jquery-3.7.1.min.js -P terminal
+wget https://huggingface.co/TheBloke/Mistral-7B-OpenOrca-GGUF/resolve/main/mistral-7b-openorca.Q5_K_M.gguf -P models
