9053522dfeaf3fa25cd6c07368bee23235ec27c5
Daniel Hogan <6313241+dphogan@users.noreply.github.com>
2024-08-09 16:45:08 -0400

    Remove obsolete download script

--- download.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p models messages logs
-wget https://code.jquery.com/jquery-3.7.1.min.js -P terminal
-wget https://huggingface.co/TheBloke/Mistral-7B-OpenOrca-GGUF/resolve/main/mistral-7b-openorca.Q5_K_M.gguf -P models
