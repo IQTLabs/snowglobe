583457896e336bfcd9f78c62ca8d14927adfa14f
Daniel Hogan <6313241+dphogan@users.noreply.github.com>
2025-03-22 18:14:37 -0400

    Form to get user ID

--- src/llm_snowglobe/interface.py
+++ src/llm_snowglobe/interface.py
@@ -1,28 +1,32 @@
 #!/usr/bin/env python3
 
 import os
-from nicegui import ui
+import asyncio
+from nicegui import ui, app
 
-interface_here = os.path.dirname(os.path.abspath(__file__))
+here = os.path.dirname(os.path.abspath(__file__))
 
-async def load_id():
-    pass
+async def load_id(idval):
+    app.storage.tab['id'] = idval
+    app.storage.tab['logged_in'] = True
+
 
 @ui.page('/')
-def interface_page():
+async def interface_page():
+    await ui.context.client.connected()
     ui.context.client.content.classes('h-screen')
     with ui.left_drawer(bordered=True).classes('items-center'):
         with ui.column(align_items='center'):
-            ui.image(os.path.join(interface_here, 'terminal/snowglobe.png')).props('width=150px').style('border-radius: 5%')
+            ui.image(os.path.join(here, 'terminal/snowglobe.png')).props('width=150px').style('border-radius: 5%')
             ui.label('User Interface').style('font-size: 25px; font-weight: bold')
             ui.button('Toggle Full Screen', on_click=ui.fullscreen().toggle)
             ui.button('Toggle Dark Mode', on_click=ui.dark_mode().toggle)
-            with ui.row():
-                ui.input('ID', placeholder='Player ID #').props('size=8')
-                ui.button('Connect', on_click=load_id)
-            with ui.row():
-                ui.label('ID')
-                ui.label('Name')
+            with ui.row().bind_visibility_from(app.storage.tab, 'logged_in', backward=lambda x: not x):
+                login_id = ui.number('ID', placeholder='Player ID #', format='%d').props('size=8')
+                ui.button('Connect', on_click=lambda x: load_id(login_id.value))
+            with ui.row().bind_visibility_from(app.storage.tab, 'logged_in'):
+                login_numb = ui.label('ID').bind_text_from(app.storage.tab, 'id')
+                login_name = ui.label('Name')
     with ui.tabs().classes('w-full') as tabs:
         chattab = ui.tab('Chat')
         infotab = ui.tab('Info')
@@ -40,7 +44,7 @@ def interface_page():
 
 def snowglobe_interface(host='0.0.0.0', port=8000):
     ui.run(host=host, port=port, title='Snow Globe User Interface',
-           favicon=os.path.join(interface_here, 'terminal/favicon.ico'),
+           favicon=os.path.join(here, 'terminal/favicon.ico'),
            reload=True)
 
 
